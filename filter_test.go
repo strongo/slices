553769fcb80b6f618a6c9db3a98f663c648bd478
Alexander Trakhimenok <alexander.trakhimenok@gmail.com>
2018-07-13 08:38:18 +0100

    refactoring & fixes

--- filter_test.go
+++ filter_test.go
@@ -8,10 +8,10 @@ func TestRemoveStrings(t *testing.T) {
 
 	for _, testCase := range []struct {
 		removed int
-		source []string
-		remove []string
-		result []string
-	} {
+		source  []string
+		remove  []string
+		result  []string
+	}{
 		{removed: 0, source: s, remove: []string{}, result: s},
 		{removed: 0, source: s, remove: []string{"v0"}, result: s},
 		{removed: 1, source: s, remove: []string{"v1"}, result: []string{"v2", "v3"}},
@@ -20,8 +20,8 @@ func TestRemoveStrings(t *testing.T) {
 		{removed: 2, source: s, remove: []string{"v1", "v2"}, result: []string{"v3"}},
 		{removed: 2, source: s, remove: []string{"v1", "v3"}, result: []string{"v2"}},
 		{removed: 2, source: s, remove: []string{"v2", "v3"}, result: []string{"v1"}},
-		{removed: 4, source: []string{"v1", "v2", "v2", "v3", "v2", "v2"}, remove: []string{"v2", }, result: []string{"v1", "v3"}},
-	}{
+		{removed: 4, source: []string{"v1", "v2", "v2", "v3", "v2", "v2"}, remove: []string{"v2",}, result: []string{"v1", "v3"}},
+	} {
 		result, removedCount := RemoveStrings(testCase.source, testCase.remove)
 		if removedCount != testCase.removed {
 			t.Errorf("RemoveStrings(%v, %v) expected to return removedCount=%v, got %v", s, testCase.remove, testCase.removed, removedCount)
@@ -37,17 +37,19 @@ func TestIsInStringSlice(t *testing.T) {
 	s := []string{"v1", "v2", "v3"}
 
 	for _, test := range []struct {
+		s        []string
 		v        string
 		expected bool
 	}{
-		{"v0", false},
-		{"v1", true},
-		{"v2", true},
-		{"v3", true},
-		{"v4", false},
+		{[]string{}, "v1", false},
+		{s, "v0", false},
+		{s, "v1", true},
+		{s, "v2", true},
+		{s, "v3", true},
+		{s, "v4", false},
 	} {
-		if IsInStringSlice(test.v, s) != test.expected {
-			t.Errorf("Expected %v for IsInStringSlice(%v, %v)", test.expected, test.v, s)
+		if IsInStringSlice(test.v, test.s) != test.expected {
+			t.Errorf("Expected %v for IsInStringSlice(%v, %v)", test.expected, test.v, test.s)
 		}
 	}
 }
