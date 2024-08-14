17f0a49880c8a61970735ff93c4c553fe81eb819
Alexander Trakhimenok <533159+trakhimenok@users.noreply.github.com>
2024-08-14 15:01:10 +0100

    feat(merge): generic implementation

--- merge.go
+++ merge.go
@@ -1,6 +1,6 @@
 package slices
 
-func MergeInt64s(source []int64, slices ...[]int64) (result []int64, changed bool) {
+func Merge[T comparable](source []T, slices ...[]T) (result []T, changed bool) {
 	result = source[:]
 	for _, slice := range slices {
 		for _, v := range slice {
@@ -17,19 +17,12 @@ func MergeInt64s(source []int64, slices ...[]int64) (result []int64, changed boo
 	return
 }
 
+// Deprecated: Use Merge instead
+func MergeInt64s(source []int64, slices ...[]int64) (result []int64, changed bool) {
+	return Merge(source, slices...)
+}
+
+// Deprecated: Use Merge instead
 func MergeStrings(source []string, slices ...[]string) (result []string, changed bool) {
-	result = source[:]
-	for _, slice := range slices {
-		for _, v := range slice {
-			for _, r := range result {
-				if r == v {
-					goto found
-				}
-			}
-			result = append(result, v)
-			changed = true
-		found:
-		}
-	}
-	return
+	return Merge(source, slices...)
 }
